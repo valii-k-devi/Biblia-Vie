d527804d4246cf62a1ef95cf69095a11bff7b73b
George Stanescu <georgestanescu@outlook.com>
2021-06-11 09:17:56 +0300

    build content dynamically

---
+++ continut.js
@@ -0,0 +1,58 @@
+var carti = [
+  {
+    "nume": "Ev Matei",
+    "capitole": [
+      {
+        "nume": "Cap 1",
+        "descriere": "Descriere",
+        "versete": [
+          "verset 1.1",
+          "verset 1.2"
+        ]
+      },
+      {
+        "nume": "Cap 2",
+        "descriere": "Descriere",
+        "versete": [
+          "verset 2.1",
+          "verset 2.2",
+          "verset 2.3"
+        ]
+      }
+    ]
+  },
+  {
+    "nume": "Ev Marcu",
+    "capitole": [
+      {
+        "nume": "Cap 1",
+        "descriere": "Descriere",
+        "versete": [
+          "verset 1.1",
+          "verset 1.2"
+        ]
+      },
+      {
+        "nume": "Cap 2",
+        "descriere": "Descriere",
+        "versete": [
+          "verset 2.1",
+          "verset 2.2",
+          "verset 2.3"
+        ]
+      },
+      {
+        "nume": "Cap 3",
+        "descriere": "Descriere",
+        "versete": [
+          "verset 3.1",
+          "verset 3.2",
+          "verset 3.3",
+          "verset 3.4",
+          "verset 3.5"
+        ]
+      }
+    ]
+  }
+];
+  
